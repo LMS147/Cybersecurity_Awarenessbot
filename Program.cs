b6a3acd3a9fd8a8d555d2491406028b434cbd767
LMS147 <mashaoleshaolin0@gmail.com>
2025-05-08 12:34:05 +0200

    with a class for conversation history and preferences

--- Program.cs
+++ Program.cs
@@ -3,13 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+using System.Media;
 
 namespace Cybersecurity_Awarenessbot
 {
+    //Represents a user's profile with conversation history and preferences
+    public class UserProfile
+    {
+        public string Name { get; set; } = "Friend";
+        public HashSet<string> Interests { get; } = new HashSet<string>();  // Tracks cybersecurity topics of interest
+        public string LastTopic { get; set; } = "";  // Maintains conversation context
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
